684daea44e346ea42ba4d90c86116caaa3676e7e
Asher Duarte <dev81log@gmail.com>
2023-02-03 08:13:47 -0300

    Update func testHandleMovies

---
+++ internal/db_connect_test.go
@@ -0,0 +1,16 @@
+package internal
+
+import "testing"
+
+func TestConnectDB(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ConnectDB()
+		})
+	}
+}
--- internal/server_test.go
+++ internal/server_test.go
@@ -1,6 +1,8 @@
 package internal
 
 import (
+	"net/http"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,4 +12,25 @@ func TestHttpServer(t *testing.T) {
 	status := HandleMovies
 	assert.NotNil(t, status, "Expected status to be not nil")
 	assert.NotEmpty(t, status, "Expected status to be not empty")
+}
+
+func TestHandleMovies(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HandleMovies(tt.args.w, tt.args.r); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("HandleMovies() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
